91303120319f33d844bbf5987927c932c6a2cb98
Ascend96 <mmartens28@yahoo.com>
2021-03-02 13:36:01 -0600

    Featured Discounts
    - Send a collection of discounts as the model for Home/Index view
    - Iterate thru view model and display discount info

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,16 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
 
 
 namespace Northwind.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        // this controller depends on the NorthwindRepository
+        private NorthwindContext _northwindContext;
+        public HomeController(NorthwindContext db) => _northwindContext = db;
+
+        public ActionResult Index() => View(_northwindContext.Discounts);
     }
 }
