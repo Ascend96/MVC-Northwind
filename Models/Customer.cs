5b143ac43cf7605b694c019b7f060cea04784f7b
Ascend96 <mmartens28@yahoo.com>
2021-04-06 13:28:12 -0500

    Modify Customer/Register View - Add form field to input email - Add form field to input password

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -12,7 +12,6 @@ namespace Northwind.Models
         public string CompanyName { get; set; }
         [Required]
         public string Email { get; set; }
-        
         public string Address { get; set; }
         public string City { get; set; }
         public string Region { get; set; }
