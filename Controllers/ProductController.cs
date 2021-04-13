719b819ec21f70b06dc2cd22da975cc405d49bd0
Ascend96 <mmartens28@yahoo.com>
2021-04-13 13:24:51 -0500

    Determine category id
    - Modify product/index controller method - category id is the model
    - Modify product/index view - remove Razor used to generate table
    - Display category id

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -11,7 +11,7 @@ namespace Northwind.Controllers
         public ProductController(NorthwindContext db) => _northwindContext = db;
 
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(p => p.CategoryName));
-        public IActionResult Index(int id) => View(_northwindContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(n => n.ProductName));
+        public IActionResult Index(int id) => View(id);
         public IActionResult Discount() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
 
     }
